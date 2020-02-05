603186cfd0f50cc686faa8b3c9dc0a6ca934a51f
Callum <54590992+Escapehub@users.noreply.github.com>
2020-02-05 22:03:42 +0000

    in

---
+++ LockScreen/NativeMethods.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LockScreen
+{
+    public partial class NativeMethods
+    {
+        [System.Runtime.InteropServices.DllImportAttribute("user32.dll", EntryPoint = "BlockInput")]
+        [return: System.Runtime.InteropServices.MarshalAsAttribute(System.Runtime.InteropServices.UnmanagedType.Bool)]
+        public static extern bool BlockInput([System.Runtime.InteropServices.MarshalAsAttribute(System.Runtime.InteropServices.UnmanagedType.Bool)] bool fBlockIt);
+    }
+}
---
+++ LockScreen/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+
+namespace LockScreen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BlockInput(SelectKey());
+        }
+        public static void BlockInput(int span)
+        {
+            try
+            {
+                NativeMethods.BlockInput(true);
+                Console.WriteLine($"Stopping input {span} miliseconds");
+                Thread.Sleep(span);
+            }
+            finally
+            {
+                Console.WriteLine("Allowing Keyboard input");
+                NativeMethods.BlockInput(false);
+            }
+        }
+        public static int SelectKey()
+        {
+            //Console.WriteLine("Please input what hotkey you would like");
+            Console.WriteLine("How long do you want to pause input (Miliseconds)");
+            return int.Parse(Console.ReadLine());
+        }
+    }
+}
